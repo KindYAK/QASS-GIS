61a849b280ab9d66d3d36df71e26ed57745f01cb
KindYAK <yakunin.k@mail.ru>
2022-04-19 17:30:08 +0600

    Small layout adjustments

--- front/front/pages/index.vue
+++ front/front/pages/index.vue
@@ -1,5 +1,6 @@
 <template>
-   <div id="map-wrap" style="height: 100vh">
+  <div>
+    <div id="map-wrap" style="height: 75vh">
       <client-only>
         <l-map :zoom=5 :center="[48.0196, 66.9237]">
           <l-tile-layer url="http://{s}.tile.osm.org/{z}/{x}/{y}.png"></l-tile-layer>
@@ -16,30 +17,31 @@
         </l-map>
       </client-only>
     </div>
+  </div>
 </template>
 
 <script>
-  import {GEOSERVER_WMS_URL} from '~/settings/dev'
+import {GEOSERVER_WMS_URL} from '~/settings/dev'
 
-  export default {
-    data() {
-      return {
-        wmsLayer: {
-          url: GEOSERVER_WMS_URL,
-          name: 'test',
-          visible: true,
-          format: 'image/png',
-          layers: ['qass:2016_prom1_0101_0301', 'qass:MCD_SI1_0121y_0401_0415_byte_0.041_0.322'],
-          transparent: false,
-          attribution: 'РГП на ПХВ "ИИВТ" КН МОН РК',
-        }
-      }
-    },
-    async asyncData({ app }) {
-      let regions = await app.$api.getRegions();
-      console.log(regions);
-      return {
+export default {
+  data() {
+    return {
+      wmsLayer: {
+        url: GEOSERVER_WMS_URL,
+        name: 'test',
+        visible: true,
+        format: 'image/png',
+        layers: ['qass:2016_prom1_0101_0301', 'qass:MCD_SI1_0121y_0401_0415_byte_0.041_0.322'],
+        transparent: false,
+        attribution: 'РГП на ПХВ "ИИВТ" КН МОН РК',
       }
     }
+  },
+  async asyncData({app}) {
+    let regions = await app.$api.getRegions();
+    return {
+      regions: regions
+    }
   }
+}
 </script>

8d7f03208293e2f89dd31ee72866bb290a0ddc5c
KindYAK <yakunin.k@mail.ru>
2022-05-05 20:24:59 +0600

    feat - basic main page

--- front/front/settings/dev.js
+++ front/front/settings/dev.js
@@ -1,1 +1,2 @@
 export const GEOSERVER_WMS_URL = 'http://localhost/geoserver/qass/wms?service=WMS';
+export const STATIC_URL = 'http://localhost/static';
--- front/front/settings/prod.js
+++ front/front/settings/prod.js
@@ -1,1 +1,2 @@
 export const GEOSERVER_WMS_URL = 'http://89.250.84.153:4814/geoserver/qass/wms?service=WMS';
+export const STATIC_URL = 'http://89.250.84.153:4814/static';

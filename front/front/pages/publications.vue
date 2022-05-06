580f3b8f185418e9aa9bb6d43276007886ee2c17
KindYAK <yakunin.k@mail.ru>
2022-05-06 13:43:19 +0600

    more publications

--- front/front/pages/publications.vue
+++ front/front/pages/publications.vue
@@ -6,6 +6,45 @@
 
     <ul style="list-style-type: decimal">
       <li class="mb-3">
+        Terekhov, A. G., Abayev, N. N., & Maglinets, Yu. A. (2021). Satellite monitoring of River Amu Darya oases during 2003–2020
+        based on irrigation cooling effect. In Sovremennye problemy distantsionnogo zondirovaniya Zemli iz kosmosa (Vol. 18, Issue 5, pp. 123–132).
+        Space Research Institute RAS. <a>https://doi.org/10.21046/2070-7401-2021-18-5-123-132</a>
+      </li>
+      <li class="mb-3">
+        Terekhov, A. G., Abayev, N. N., & Tillakarim, T. A. (2021). Extremal snowfall on March 13–14, 2021, in the South
+        Kazakhstan. In Sovremennye problemy distantsionnogo zondirovaniya Zemli iz kosmosa (Vol. 18, Issue 4, pp. 279–284).
+        Space Research Institute RAS. <a>https://doi.org/10.21046/2070-7401-2021-18-4-279-284</a>
+      </li>
+      <li class="mb-3">
+        Abayev, N. N. (2022). The relationship between the Ili River streamflow and operation modes of the reservoirs in the upper 
+        river basin. In Sovremennye problemy distantsionnogo zondirovaniya Zemli iz kosmosa (Vol. 19, Issue 1, pp. 217–225). 
+        Space Research Institute RAS. <a>https://doi.org/10.21046/2070-7401-2022-19-1-217-225</a> 
+      </li>
+      <li class="mb-3">
+        Terekhov, A., & Abayev, N. (2021). About disappearance of a large Lake Ebi-Nur, Xinjiang, China. In G. M. Tsybulski 
+        Gennady, M. V. Noskov, & Yu. A. Maglinets (Eds.), E3S Web of Conferences (Vol. 333, p. 02016). EDP Sciences. <a>https://doi.org/10.1051/e3sconf/202133302016</a> 
+      </li>
+      <li class="mb-3">
+        TEREKHOV, A., ABAYEV, N., & LAGUTIN, E. (2021). PATTERNS OF THE CENTRAL ASIA SNOW COVER DURING 2001-2021. In 
+        Sustainable Development of Mountain Territories (Vol. 13, Issue 4, pp. 497–504). FSBEIHE North Caucasian Institute 
+        of Mining and Metallurgy (STU). <a>https://doi.org/10.21177/1998-4502-2021-13-4-497-504</a> 
+      </li>
+      <li class="mb-3">
+        Терехов А.Г., Абаев Н.Н., Амиргалиев Е.Н. Спутниковое обследование емкости Шардаринского водохранилища (Казахстан)
+        на реке Сырдарья по данным Landsat-8 и Sentinel-2A за 2020-2021 гг. // Материалы 19-й Международной конференции
+        «Современные проблемы дистанционного зондирования Земли из космоса» Москва, ИКИ РАН, 15–19 ноября 2021 г. с.114. DOI 10.21046/19DZZconf-2021a
+      </li>
+      <li class="mb-3">
+        Терехов А.Г.,, Амиргалиев Е.Н., Абаев Н.Н., Маглинец Ю.А. Сравнительный анализ водообеспеченности оазисов бассейна
+        р. Амударья в период 2003-2021 гг. по данным LST FEWS NET// Материалы 19-й Международной конференции «Современные
+        проблемы дистанционного зондирования Земли из космоса» Москва, ИКИ РАН, 15–19 ноября 2021 г. с.115. DOI 10.21046/19DZZconf-2021a
+      </li>
+      <li class="mb-3">
+        Терехов А.Г., Абаев Н.Н., Амиргалиев Е.Н. Спутниковый мониторинг гидрометаллургической активности на дне высохшего
+        озера Манас расположенного на крупном нефтегазовом месторождении Карамай (СУАР КНР) // Материалы 19-й Международной
+        конференции «Современные проблемы дистанционного зондирования Земли из космоса» Москва, ИКИ РАН, 15–19 ноября 2021 г. с.330. DOI 10.21046/19DZZconf-2021a
+      </li>
+      <li class="mb-3">
         Терехов А. Г., Абаев Н. Н., Тиллакарим Т. А. Явление аномального снегопада 13–14 марта 2021 г. на юге Казахстана
         //Современные проблемы дистанционного зондирования Земли из космоса. – 2021. – Т. 18. – №. 4. – С. 279-284. <a>http://d33.infospace.ru/jr_d33/2021v18n4/279-284.pdf</a>
       </li>

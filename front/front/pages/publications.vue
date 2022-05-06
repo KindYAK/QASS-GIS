5d9ee2fd65f99e8fc65c1160faeff9ed574010a8
KindYAK <yakunin.k@mail.ru>
2022-05-06 14:53:52 +0600

    fix

--- front/front/pages/publications.vue
+++ front/front/pages/publications.vue
@@ -8,26 +8,26 @@
       <li class="mb-3">
         Terekhov, A. G., Abayev, N. N., & Maglinets, Yu. A. (2021). Satellite monitoring of River Amu Darya oases during 2003–2020
         based on irrigation cooling effect. In Sovremennye problemy distantsionnogo zondirovaniya Zemli iz kosmosa (Vol. 18, Issue 5, pp. 123–132).
-        Space Research Institute RAS. <a src='https://doi.org/10.21046/2070-7401-2021-18-5-123-132'>https://doi.org/10.21046/2070-7401-2021-18-5-123-132</a>
+        Space Research Institute RAS. <a href='https://doi.org/10.21046/2070-7401-2021-18-5-123-132'>https://doi.org/10.21046/2070-7401-2021-18-5-123-132</a>
       </li>
       <li class="mb-3">
         Terekhov, A. G., Abayev, N. N., & Tillakarim, T. A. (2021). Extremal snowfall on March 13–14, 2021, in the South
         Kazakhstan. In Sovremennye problemy distantsionnogo zondirovaniya Zemli iz kosmosa (Vol. 18, Issue 4, pp. 279–284).
-        Space Research Institute RAS. <a src='https://doi.org/10.21046/2070-7401-2021-18-4-279-284'>https://doi.org/10.21046/2070-7401-2021-18-4-279-284</a>
+        Space Research Institute RAS. <a href='https://doi.org/10.21046/2070-7401-2021-18-4-279-284'>https://doi.org/10.21046/2070-7401-2021-18-4-279-284</a>
       </li>
       <li class="mb-3">
         Abayev, N. N. (2022). The relationship between the Ili River streamflow and operation modes of the reservoirs in the upper
         river basin. In Sovremennye problemy distantsionnogo zondirovaniya Zemli iz kosmosa (Vol. 19, Issue 1, pp. 217–225).
-        Space Research Institute RAS. <a src='https://doi.org/10.21046/2070-7401-2022-19-1-217-225'>https://doi.org/10.21046/2070-7401-2022-19-1-217-225</a>
+        Space Research Institute RAS. <a href='https://doi.org/10.21046/2070-7401-2022-19-1-217-225'>https://doi.org/10.21046/2070-7401-2022-19-1-217-225</a>
       </li>
       <li class="mb-3">
         Terekhov, A., & Abayev, N. (2021). About disappearance of a large Lake Ebi-Nur, Xinjiang, China. In G. M. Tsybulski
-        Gennady, M. V. Noskov, & Yu. A. Maglinets (Eds.), E3S Web of Conferences (Vol. 333, p. 02016). EDP Sciences. <a src='https://doi.org/10.1051/e3sconf/202133302016'>https://doi.org/10.1051/e3sconf/202133302016</a>
+        Gennady, M. V. Noskov, & Yu. A. Maglinets (Eds.), E3S Web of Conferences (Vol. 333, p. 02016). EDP Sciences. <a href='https://doi.org/10.1051/e3sconf/202133302016'>https://doi.org/10.1051/e3sconf/202133302016</a>
       </li>
       <li class="mb-3">
         TEREKHOV, A., ABAYEV, N., & LAGUTIN, E. (2021). PATTERNS OF THE CENTRAL ASIA SNOW COVER DURING 2001-2021. In
         Sustainable Development of Mountain Territories (Vol. 13, Issue 4, pp. 497–504). FSBEIHE North Caucasian Institute
-        of Mining and Metallurgy (STU). <a src='https://doi.org/10.21177/1998-4502-2021-13-4-497-504'>https://doi.org/10.21177/1998-4502-2021-13-4-497-504</a>
+        of Mining and Metallurgy (STU). <a href='https://doi.org/10.21177/1998-4502-2021-13-4-497-504'>https://doi.org/10.21177/1998-4502-2021-13-4-497-504</a>
       </li>
       <li class="mb-3">
         Терехов А.Г., Абаев Н.Н., Амиргалиев Е.Н. Спутниковое обследование емкости Шардаринского водохранилища (Казахстан)
@@ -46,20 +46,20 @@
       </li>
       <li class="mb-3">
         Терехов А. Г., Абаев Н. Н., Тиллакарим Т. А. Явление аномального снегопада 13–14 марта 2021 г. на юге Казахстана
-        //Современные проблемы дистанционного зондирования Земли из космоса. – 2021. – Т. 18. – №. 4. – С. 279-284. <a src='http://d33.infospace.ru/jr_d33/2021v18n4/279-284.pdf'>http://d33.infospace.ru/jr_d33/2021v18n4/279-284.pdf</a>
+        //Современные проблемы дистанционного зондирования Земли из космоса. – 2021. – Т. 18. – №. 4. – С. 279-284. <a href='http://d33.infospace.ru/jr_d33/2021v18n4/279-284.pdf'>http://d33.infospace.ru/jr_d33/2021v18n4/279-284.pdf</a>
       </li>
       <li class="mb-3">
         Терехов А.Г., Абаев Н.Н., Ю.А. Маглинец Спутниковый мониторинг состояния оазисов реки Амударьи в период 2003–2020 гг.
         на основе анализа эффекта охлаждения территорий в результате их ирригации. Современные проблемы дистанционного зондирования Земли из космоса.
-        2021. Т. 18. №5. С. 123–132. <a src='http://d33.infospace.ru/jr_d33/2021v18n5/123-132.pdf'>http://d33.infospace.ru/jr_d33/2021v18n5/123-132.pdf</a>
+        2021. Т. 18. №5. С. 123–132. <a href='http://d33.infospace.ru/jr_d33/2021v18n5/123-132.pdf'>http://d33.infospace.ru/jr_d33/2021v18n5/123-132.pdf</a>
       </li>
       <li class="mb-3">
         Mukhamediev R. I. et al. Review of Some Applications of Unmanned Aerial Vehicles Technology in the Resource-Rich
-        Country //Applied Sciences. – 2021. – Т. 11. – №. 21. – С. 10171.  <a src='https://doi.org/10.3390/app112110171'>https://doi.org/10.3390/app112110171</a>
+        Country //Applied Sciences. – 2021. – Т. 11. – №. 21. – С. 10171.  <a href='https://doi.org/10.3390/app112110171'>https://doi.org/10.3390/app112110171</a>
       </li>
       <li class="mb-3">
         Kong M. et al. Energy-efficient rescheduling with time-of-use energy cost: Application of variable neighborhood
-        search algorithm //Computers & Industrial Engineering. – 2021. – Т. 156. – С. 107286. <a src='https://doi.org/10.1016/j.cie.2021.107286'>https://doi.org/10.1016/j.cie.2021.107286</a>
+        search algorithm //Computers & Industrial Engineering. – 2021. – Т. 156. – С. 107286. <a href='https://doi.org/10.1016/j.cie.2021.107286'>https://doi.org/10.1016/j.cie.2021.107286</a>
       </li>
       <li class="mb-3">
         Мустафин С. Определение момента начала эксплуатации закладки Свидетельство о депонировании результатов интеллектуальной

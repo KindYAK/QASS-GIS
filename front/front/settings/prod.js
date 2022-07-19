83e31ff1b948aac1fdf56914c8bd40b34180705d
KindYAK <yakunin.k@mail.ru>
2022-07-19 19:16:05 +0600

    front - /geoserver/ - HTTPS -> HTTP

--- front/front/settings/prod.js
+++ front/front/settings/prod.js
@@ -1,4 +1,4 @@
-export const GEOSERVER_WMS_URL = 'https://qass.iict.kz/geoserver/qass/wms?service=WMS';
+export const GEOSERVER_WMS_URL = 'http://qass.iict.kz/geoserver/qass/wms?service=WMS';
 export const STATIC_URL = 'https://qass.iict.kz/static/';
 export const MEDIA_URL = 'https://qass.iict.kz/media/';
 export const ROOT_API = 'https://qass.iict.kz/api/'

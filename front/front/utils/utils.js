c30e629b57d5a370d9deb938918136eabc9bdd86
Kirill Yakunin <yakunin.k@mail.ru>
2022-11-23 00:15:23 +0600

    fix attempt

--- front/front/utils/utils.js
+++ front/front/utils/utils.js
@@ -1,6 +1,13 @@
 export function redrawLastLayer(layers) {
-  let n = Object.keys(layers)[Object.keys(layers).length - 1]
-  layers[n].redraw();
+  for(let i = 0; i <= 5; i++) {
+    try {
+      let n = Object.keys(layers)[Object.keys(layers).length - 1]
+      layers[n].redraw();
+    } catch {
+      continue
+    }
+    break
+  }
 }
 
 export function includesLayer(layers, layer){

b6dc33292ada020d6fbff596dbe8d330d3595475
Kirill Yakunin <yakunin.k@mail.ru>
2023-06-24 20:23:44 +0600

    search

--- front/front/components/layersMenu.vue
+++ front/front/components/layersMenu.vue
@@ -4,6 +4,15 @@
       style="margin-left: 15px;"
       @click="layersFromMenu = []"
     >Сбросить выбор</v-btn>
+
+    <v-text-field
+      v-model="search"
+      append-icon="mdi-magnify"
+      label="Search"
+      single-line
+      hide-details
+    ></v-text-field>
+
     <v-treeview
       hoverable
       dense
@@ -12,7 +21,7 @@
       @input="changeLayers"
       item-disabled="locked"
       selection-type="independent"
-      :items="regions"
+      :items="filteredRegions"
     ></v-treeview>
   </div>
 </template>
@@ -24,14 +33,69 @@ export default {
   data() {
     return {
       layersFromMenu: [],
+      search: '',
     }
   },
   props: [
     'regions'
   ],
+  computed: {
+    filteredRegions() {
+      const search = this.search.toLowerCase();
+
+      return this.regions.reduce((acc, region) => {
+        // Check if region's name includes the search term
+        let isRegionNameIncludesSearch = region.name.toLowerCase().includes(search);
+
+        // Check if any of region's children's name includes the search term
+        let filteredChildren = this.filterChildren(region.children, search, isRegionNameIncludesSearch);
+
+        if (isRegionNameIncludesSearch || filteredChildren.length > 0) {
+          // Clone the region to avoid mutation
+          let newRegion = { ...region };
+
+          // Assign filtered children to new region
+          newRegion.children = filteredChildren;
+
+          acc.push(newRegion);
+        }
+
+        return acc;
+      }, []);
+    }
+  },
   methods: {
-    changeLayers({store}){
+    changeLayers(){
       this.$store.commit('SET_LAYERS_CHOSEN', this.layersFromMenu)
+    },
+    filterChildren(children, search, parentMatch) {
+      if (!children || children.length === 0) {
+        return [];
+      }
+
+      return children.reduce((acc, child) => {
+        let isChildNameIncludesSearch = child.name.toLowerCase().includes(search);
+
+        if (isChildNameIncludesSearch || parentMatch) {
+          // Clone the child to avoid mutation
+          let newChild = {...child};
+          // If the child's name matches the search term or parentMatch is true,
+          // add all of its children without filtering.
+          newChild.children = child.children;
+          acc.push(newChild);
+        } else {
+          // If the child's name doesn't match the search term and parentMatch is false,
+          // filter the child's children that match the search term.
+          let filteredGrandchildren = this.filterChildren(child.children, search, false);
+          if (filteredGrandchildren.length > 0) {
+            let newChild = {...child};
+            newChild.children = filteredGrandchildren;
+            acc.push(newChild);
+          }
+        }
+
+        return acc;
+      }, []);
     }
   }
 };
